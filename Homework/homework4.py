17def7ce6339589f4e1319891eeef8275a98d6ba
NSC508 <46616024+NSC508@users.noreply.github.com>
2022-12-01 00:32:30 +0000

    Finished up till A9

--- Homework/homework4.py
+++ Homework/homework4.py
@@ -16,8 +16,8 @@ e1 = -30 * np.ones(n)
 e2 = 16 * np.ones(n)
 e3 = -1 * np.ones(n)
 A = scipy.sparse.spdiags([e3, e2, e1, e2, e3],
-                         [-2, -1, 0, 1, 2], n, n)
-A = scipy.sparse.lil_matrix(A)
+                         [-2, -1, 0, 1, 2], n, n, format='csc')
+#A = scipy.sparse.lil_matrix(A)
 A[0, -1] = 16
 A[0, -2] = -1
 A[1, -1] = -1
@@ -37,7 +37,8 @@ for i in range(len(tspan) - 1):
     sol1[:, i + 1] = u1 
     u0 = u1
 A5 = sol1[:, -1]
-print(A5)
+#reshape A5 to a 128x1 matrix
+A5 = A5.reshape(128,1)
 # %%
 # stability analysis
 g = lambda z: 1 + 1/12 * lambda_star * (-30 + 32 * np.cos(z) - 2 * np.cos(2 * z))
@@ -50,13 +51,37 @@ A2 = g(minimum_index)
 e1 = -2 * np.ones(n)
 e2 = np.ones(n)
 mid = 1/2 * lambda_star * scipy.sparse.spdiags([e2, e1, e2],
-                                              [-1, 0, 1], n, n)
-mid = scipy.sparse.lil_matrix(mid)
-mid[0, -1] = 1
-mid[-1, 0] = 1
+                                              [-1, 0, 1], n, n, format='csc')
+#mid = scipy.sparse.lil_matrix(mid)
+mid[0, -1] = 1/2 * lambda_star
+mid[-1, 0] = 1/2 * lambda_star
 B = scipy.sparse.eye(n) - mid
 C = scipy.sparse.eye(n) + mid
 A7 = B.toarray()
 A8 = C.toarray()
+print(A8)
+# %%
+# Solve suing LU decomposition 
+sol2 = np.zeros((len(xspan), len(tspan)))
+u0 = 10 * np.cos(2 * np.pi * xspan / L) + 30 * np.cos(8 * np.pi * xspan / L)
+sol2[:, 0] = u0
+PLU = scipy.sparse.linalg.splu(B)
+test = PLU.solve(C@u0)
+for i in range(len(tspan) - 1):
+    u1 = PLU.solve(C@u0)
+    sol2[:, i + 1] = u1
+    u0 = u1
+A9 = sol2[:, -1]
+# reshape A9 to a 128x1 matrix
+A9 = A9.reshape(128,1)
+print(A9)
+
+A10 = 0 
 # %%
 # Stability analysis
+g = lambda z: (1 + lambda_star * (np.cos(z) - 1)) / (1 - lambda_star * (np.cos(z) - 1))
+minimum_index = scipy.optimize.fminbound(lambda z: -abs(g(z)), -np.pi, np.pi)
+A6 = g(minimum_index)
+
+# %%
+# Question 3
