e9383a2641e5be9e7f264b0cfaec3cd6e558ea19
NSC508 <46616024+NSC508@users.noreply.github.com>
2022-12-01 01:17:22 +0000

    finished all of HW4

--- Homework/homework4.py
+++ Homework/homework4.py
@@ -52,7 +52,7 @@ e1 = -2 * np.ones(n)
 e2 = np.ones(n)
 mid = 1/2 * lambda_star * scipy.sparse.spdiags([e2, e1, e2],
                                               [-1, 0, 1], n, n, format='csc')
-#mid = scipy.sparse.lil_matrix(mid)
+
 mid[0, -1] = 1/2 * lambda_star
 mid[-1, 0] = 1/2 * lambda_star
 B = scipy.sparse.eye(n) - mid
@@ -85,3 +85,69 @@ A6 = g(minimum_index)
 
 # %%
 # Question 3
+#Load the exact solution from the file exact_128.csv 
+exact = np.loadtxt('exact_128.csv', delimiter=',')
+#reshape the exact solution to a 128x1 matrix
+exact = exact.reshape(128,1)
+#find the two norm difference between A5 and exact, save as A11
+A11 = np.linalg.norm(A5 - exact)
+#find the two norm difference between A9 and exact, save as A12
+A12 = np.linalg.norm(A9 - exact)
+
+# %%
+# redo both problems with n = 256, load in the exact solution from the file exact_256.csv
+n = 256
+xspan, dx = np.linspace(-L, L, n, endpoint=False, retstep=True)
+tspan, dt = np.linspace(0, Time, (4 * 500) + 1, retstep=True)
+new_lambda_star = (alpha * dt) / (dx**2)
+e1 = -30 * np.ones(n)
+e2 = 16 * np.ones(n)
+e3 = -1 * np.ones(n)
+A = scipy.sparse.spdiags([e3, e2, e1, e2, e3],
+                         [-2, -1, 0, 1, 2], n, n, format='csc')
+
+A[0, -1] = 16
+A[0, -2] = -1
+A[1, -1] = -1
+A[-1, 0] = 16
+A[-1, 1] = -1
+A[-2, 0] = -1
+
+A = 1/12 * A
+print(A.toarray())
+sol1 = np.zeros((len(xspan), len(tspan)))
+u0 = 10 * np.cos(2 * np.pi * xspan / L) + 30 * np.cos(8 * np.pi * xspan / L)
+sol1[:, 0] = u0
+for i in range(len(tspan) - 1):
+    u1 = u0 + lambda_star * (A@u0)
+    sol1[:, i + 1] = u1 
+    u0 = u1
+first = sol1[:, -1]
+# %%
+e1 = -2 * np.ones(n)
+e2 = np.ones(n)
+mid = 1/2 * lambda_star * scipy.sparse.spdiags([e2, e1, e2],
+                                                [-1, 0, 1], n, n, format='csc')
+#mid = scipy.sparse.lil_matrix(mid)
+mid[0, -1] = 1/2 * lambda_star
+mid[-1, 0] = 1/2 * lambda_star
+B = scipy.sparse.eye(n) - mid
+C = scipy.sparse.eye(n) + mid
+# Solve suing LU decomposition
+sol2 = np.zeros((len(xspan), len(tspan)))
+u0 = 10 * np.cos(2 * np.pi * xspan / L) + 30 * np.cos(8 * np.pi * xspan / L)
+sol2[:, 0] = u0
+PLU = scipy.sparse.linalg.splu(B)
+for i in range(len(tspan) - 1):
+    u1 = PLU.solve(C@u0)
+    sol2[:, i + 1] = u1
+    u0 = u1
+second = sol2[:, -1]
+#Load the exact solution from the file exact_256.csv
+exact = np.loadtxt('exact_256.csv', delimiter=',')
+#find the two norm difference between A5 and exact, save as A11
+A13 = np.linalg.norm(first - exact)
+#find the two norm difference between second and exact, save as A12
+A14 = np.linalg.norm(second - exact)
+
+# %%
